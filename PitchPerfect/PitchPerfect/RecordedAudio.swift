7b5c27bb63964ea6a5093ebb10d1f07148c6c501
Stephen Fong <stevesf1234@gmail.com>
2015-03-08 15:28:34 -0700

    Committed by Udacity

---
+++ PitchPerfect/PitchPerfect/RecordedAudio.swift
@@ -0,0 +1,17 @@
+//
+//  RecordedAudio.swift
+//  PitchPerfect
+//
+//  Created by Stephen Fong on 3/7/15.
+//  Copyright (c) 2015 Stephen Fong. All rights reserved.
+//
+
+import UIKit
+
+class RecordedAudio: NSObject {
+    
+    var filePathUrl: NSURL!
+    var title: String!
+    
+   
+}
